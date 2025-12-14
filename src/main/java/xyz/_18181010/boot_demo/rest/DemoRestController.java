8cdbb488b210522d8a24609e69bb15e248c8c83c
guava <yuvayashvin@gmail.com>
2025-12-14 15:04:15 +0530

    explored @Value annotations

--- src/main/java/xyz/_18181010/boot_demo/rest/DemoRestController.java
+++ src/main/java/xyz/_18181010/boot_demo/rest/DemoRestController.java
@@ -1,6 +1,7 @@
 package xyz._18181010.boot_demo.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class DemoRestController {
 
+    @Value("${admin.name}")
+    private String adminName;
+
     // exposing '/' to return a string
     @GetMapping("hello")
     public String getMethodName(@RequestParam String name) {
-        return new String("Hello " + name + "!");
+        return new String("Hello " + name + "!\n\t-" + adminName);
     }
     
 }
