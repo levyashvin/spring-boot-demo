d69ee58a7f8087fa6a1dd50b5d13ba328367ab2b
guava <yuvayashvin@gmail.com>
2025-12-16 00:51:54 +0530

    added lifecycle hooks

--- src/main/java/xyz/_18181010/boot_demo/common/DerivedClass1.java
+++ src/main/java/xyz/_18181010/boot_demo/common/DerivedClass1.java
@@ -5,14 +5,29 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 @Lazy
 //@Scope("prototype")
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class DerivedClass1 implements BaseClass{
 
     DerivedClass1(){
         System.out.println("DerivedClass1 intantiated");
+    }
+
+    // postconstruct for startup tasks
+    @PostConstruct
+    void doStartUp(){
+        System.out.println("starting up "+ getClass().getSimpleName());
+    }
+
+    // tasks before bean deletion
+    @PreDestroy
+    void doCleanUp(){
+        System.out.println("cleaning up "+ getClass().getSimpleName());
     }
 
     @Override
