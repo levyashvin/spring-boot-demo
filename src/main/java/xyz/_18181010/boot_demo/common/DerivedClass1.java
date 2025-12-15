c0bf115d9d88248949776aee98bf9563c02fdf9e
guava <yuvayashvin@gmail.com>
2025-12-16 00:00:58 +0530

    added lazy evalutaion

--- src/main/java/xyz/_18181010/boot_demo/common/DerivedClass1.java
+++ src/main/java/xyz/_18181010/boot_demo/common/DerivedClass1.java
@@ -1,9 +1,15 @@
 package xyz._18181010.boot_demo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class DerivedClass1 implements BaseClass{
+
+    DerivedClass1(){
+        System.out.println("DerivedClass1 intantiated");
+    }
 
     @Override
     public String getData(){
--- src/main/java/xyz/_18181010/boot_demo/common/DerivedClass2.java
+++ src/main/java/xyz/_18181010/boot_demo/common/DerivedClass2.java
@@ -1,11 +1,16 @@
 package xyz._18181010.boot_demo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
 public class DerivedClass2 implements BaseClass{
+
+    DerivedClass2(){
+        System.out.println("DerivedClass2 intantiated");
+    }
 
     @Override
     public String getData(){
