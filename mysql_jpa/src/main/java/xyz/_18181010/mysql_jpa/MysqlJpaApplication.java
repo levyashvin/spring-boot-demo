ae528af23a5090666dccdc95967bbb433a0ce3c2
guava <yuvayashvin@gmail.com>
2025-12-17 00:08:25 +0530

    added commandlineRunner

--- mysql_jpa/src/main/java/xyz/_18181010/mysql_jpa/MysqlJpaApplication.java
+++ mysql_jpa/src/main/java/xyz/_18181010/mysql_jpa/MysqlJpaApplication.java
@@ -1,13 +1,22 @@
 package xyz._18181010.mysql_jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MysqlJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlJpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String args[]) {
+		return runner_args -> {
+			System.out.println("command line runner started.");
+		};
 	}
 
 }
